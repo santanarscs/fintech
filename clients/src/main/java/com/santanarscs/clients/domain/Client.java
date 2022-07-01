2992b32d7243c3f2233c0bb5c6e787a510ee9594
santanarscs <raphaelstn@gmail.com>
2022-07-01 17:44:43 -0300

    first commit

---
+++ clients/src/main/java/com/santanarscs/clients/domain/Client.java
@@ -0,0 +1,25 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Client {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column
+  private String cpf;
+  @Column
+  private String name;
+  @Column
+  private Integer age;
+
+}
