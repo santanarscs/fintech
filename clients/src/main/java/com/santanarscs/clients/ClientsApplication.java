1a8e409d0c5d3394dade90ae2cceb3253772c95b
santanarscs <raphaelstn@gmail.com>
2022-07-01 20:49:59 -0300

    add cloud gateway

--- clients/src/main/java/com/santanarscs/clients/ClientsApplication.java
+++ clients/src/main/java/com/santanarscs/clients/ClientsApplication.java
@@ -2,9 +2,10 @@ package com.santanarscs.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-// @EnableEurekaClient
+@EnableEurekaClient
 public class ClientsApplication {
 
 	public static void main(String[] args) {
---
+++ cloudgateway/src/test/java/com/santanarscs/cloudgateway/CloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santanarscs.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
